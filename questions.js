f636c0f6160152f42204cdf372dc4bc97a748fa4
Fernando Cruz <nando33_2005@hotmail.com>
2024-05-30 23:10:04 -0300

    Criando um Quiz
    Criando um Quiz com HTML, CSS e Javascript.

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question:"O que significa HTML?",
+        answer: "C. Linguagem de marcação de hipertexto",
+        options: [
+            "A. Hiper Tipo Multilínguas",
+            "B. Hipertexto em vários idiomas",
+            "C. Linguagem de marcação de hipertexto",
+            "D. Texto inicial em vários idiomas"
+        ]
+    },
+    {
+        numb: 2,
+        question:"O que significa CSS?",
+        answer: "A. Folha de estilo cascata",
+        options: [
+            "A. Folha de estilo cascata",
+            "B. Computer Style Sheet",
+            "C. Folha de estilo de computador",
+            "D. Folha de estilo de código"
+        ]
+    },
+    {
+        numb: 3,
+        question:"O que significa PHP?",
+        answer: "A. Pré-processador de hipertexto",
+        options: [
+            "A. Pré-processador de hipertexto",
+            "B. Programação de texto inicial",
+            "C. Pré-programação de hipertexto",
+            "D. Programando pré-processador de hipertexto"
+        ]
+    },
+    {
+        numb: 4,
+        question:"O que significa SQL?",
+        answer: "D. Linguagem de consulta estruturada",
+        options: [
+            "A. Linguagem de consulta de força",
+            "B. Linguagem de consulta de folha de estilo",
+            "C. Linguagem de perguntas científicas",
+            "D. Linguagem de consulta estruturada"
+        ]
+    },
+    {
+        numb: 5,
+        question:"O que significa XML?",
+        answer: "D. Linguagem de marcação extensível",
+        options: [
+            "A. Excelente Múltipla Linguagem",
+            "B. Explore várias linguagens",
+            "C. Linguagem de marcação extra",
+            "D. Linguagem de marcação extensível"
+        ]
+    }
+]
